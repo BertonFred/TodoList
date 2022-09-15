e1d03a708f5b602455faa931d5360d2791d3c98c
Berton Frederic <berton.frederic@gmail.com>
2022-09-15 08:49:14 +0200

    implmentation des attributs en fonction des methode

--- REST/wsRestTodoList/Controllers/TodoListController.cs
+++ REST/wsRestTodoList/Controllers/TodoListController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+// For more information on enabling Web API for empty projects, visit : https://go.microsoft.com/fwlink/?LinkID=397860
 // for information on return value : https://medium.com/awesome-net/web-api-return-types-in-net-94715415ae88
+// Scott Hanselman. ASP.NET Core RESTful Web API versioning made easy : http://www.hanselman.com/blog/ASPNETCoreRESTfulWebAPIVersioningMadeEasy.aspx
+// ASP.NET Web API Help Pages using Swagger : https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-help-pages-using-swagger
 
 namespace wsRestTodoList.Controllers
 {
@@ -41,31 +43,44 @@ namespace wsRestTodoList.Controllers
         }
 
         // GET LIST by GET request with no url arg
-        // GET: api/v1/TodoList/GetTodoItems
+        // GET: api/v1/TodoList/GetTodoItems[?pageSize=30&pageIndex=10]
         // OPENAPI OperationId = Name
-        [HttpGet(Name="GetTodoItems")]
-        public IEnumerable<TodoItem> GetTodoItems()
+        //[HttpGet(Name="GetTodoItems")]
+        [HttpGet()]
+        [Route("[action]")]
+        [Produces("application/json")]
+        [ProducesResponseType(404)] // NotFound
+        [ProducesResponseType(200)] // Success
+        public ActionResult<IEnumerable<TodoItem>> GetTodoItems([FromQuery]int pageSize = 10,[FromQuery] int pageIndex = 0)
         {
-            return Datas;
+            if (Datas == null)
+                return NotFound($"Pas de données dans la liste");
+
+            List<TodoItem> datas = Datas.Skip(pageSize * pageIndex)
+                                        .Take(pageSize)
+                                        .ToList();
+            return Ok(datas);
         }
 
         // READ by GET request with url arg for ID
         // GET api/v1/TodoList/GetTodoItem/5
         // OPENAPI OperationId = Name
         // Data in result
-        [HttpGet("GetTodoItem/{id}", Name="GetTodoItem")]
-        [Produces("applicagtion/json")]
+        //        [HttpGet("GetTodoItem/{id}", Name="GetTodoItem")]
+        [HttpGet()]
+        [Route("[action]/{id}")]
+        [Produces("application/json")]
         [ProducesResponseType(400)] // BadRequest
         [ProducesResponseType(404)] // NotFound
-        [ProducesResponseType(200, Type = typeof(TodoItem))]
-        public ActionResult GetTodoItem(int id)
+        [ProducesResponseType(200)]
+        public ActionResult<TodoItem> GetTodoItem(int id)
         {
             if (id <= 0) 
                 return BadRequest("Id doit être supérieur à 0.");
 
             TodoItem? data = Datas.FirstOrDefault(item => item.ID == id);
             if (data == null) 
-                return NotFound($"Non trouvé avec l'ID={id}");
+                return NotFound($"Non trouvé avec l'ID = {id}");
 
             return Ok(data);
         }
@@ -75,7 +90,7 @@ namespace wsRestTodoList.Controllers
         // OPENAPI OperationId = Name
         // Data in BODY
         [HttpPost("CreateTodoItem",Name="CreateTodoItem")]
-        [Produces("applicagtion/json")]
+        [Produces("application/json")]
         [ProducesResponseType(200, Type = typeof(TodoItem))]
         public ActionResult CreateTodoItem([FromBody] CreateOrUpdateTodoItem dataToAdd)
         {
@@ -97,27 +112,45 @@ namespace wsRestTodoList.Controllers
         // OPENAPI OperationId = Name
         // Data in BODY
         [HttpPut("UpdateTodoItem/{id}", Name = "UpdateTodoItem")]
-        public void UpdateTodoItem(int id, [FromBody] CreateOrUpdateTodoItem dataToUpdate)
+        [Produces("application/json")]
+        [ProducesResponseType(400)] // BadRequest
+        [ProducesResponseType(404)] // NotFound
+        [ProducesResponseType(200)]
+        public ActionResult UpdateTodoItem(int id, [FromBody] CreateOrUpdateTodoItem dataToUpdate)
         {
+            if (id <= 0)
+                return BadRequest("Id doit être supérieur à 0.");
+
             TodoItem? data = Datas.FirstOrDefault(item => item.ID == id);
-            if (data != null) 
-            {
-                data.Titre = dataToUpdate.Titre;
-                data.Description = dataToUpdate.Description;
-            }
+            if (data == null)
+                return NotFound($"Non trouvé avec l'ID = {id}");
+
+            data.Titre = dataToUpdate.Titre;
+            data.Description = dataToUpdate.Description;
+
+            return Ok();
         }
 
         // DELETE by DELETE request with url arg for ID
         // DELETE api/TodoList>/DeleteTodoItem/5
         // OPENAPI OperationId = Name
         [HttpDelete("DeleteTodoItem/{id}", Name = "DeleteTodoItem")]
-        public void DeleteTodoItem(int id)
+        [Produces("application/json")]
+        [ProducesResponseType(400)] // BadRequest
+        [ProducesResponseType(404)] // NotFound
+        [ProducesResponseType(200)]
+        public ActionResult DeleteTodoItem(int id)
         {
+            if (id <= 0)
+                return BadRequest("Id doit être supérieur à 0.");
+
             TodoItem? data = Datas.FirstOrDefault(item => item.ID == id);
-            if (data != null)
-            {
-                Datas.Remove(data);
-            }
+            if (data == null)
+                return NotFound($"Non trouvé avec l'ID = {id}");
+
+            Datas.Remove(data);
+
+            return Ok();
         }
     }
 }
